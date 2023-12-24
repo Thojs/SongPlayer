ec62912f8d998fef2bcfd0aaacadd52e134c2407
Thijs <thijs@serne.com>
2023-12-24 12:06:55 +0100

    Register commands & stage-types in main class.

--- src/main/java/com/github/hhhzzzsss/songplayer/SongPlayer.java
+++ src/main/java/com/github/hhhzzzsss/songplayer/SongPlayer.java
@@ -1,7 +1,13 @@
 package com.github.hhhzzzsss.songplayer;
 
-import com.github.hhhzzzsss.songplayer.commands.CommandProcessor;
+import com.github.hhhzzzsss.songplayer.commands.SongPlayerCommand;
+import com.github.hhhzzzsss.songplayer.stage.DefaultStage;
+import com.github.hhhzzzsss.songplayer.stage.SphericalStage;
+import com.github.hhhzzzsss.songplayer.stage.StageTypeRegistry;
+import com.github.hhhzzzsss.songplayer.stage.WideStage;
+import com.github.hhhzzzsss.songplayer.utils.Util;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
@@ -12,13 +18,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class SongPlayer implements ModInitializer {
-
 	public static final MinecraftClient MC = MinecraftClient.getInstance();
 	public static final int NOTEBLOCK_BASE_ID = Block.getRawIdFromState(Blocks.NOTE_BLOCK.getDefaultState())-1;
 
-	public static final Path SONG_DIR = Path.of("songs");
 	public static final Path SONGPLAYER_DIR = Path.of("SongPlayer");
-	public static final Path PLAYLISTS_DIR = Path.of("SongPlayer/playlists");
+	public static final Path PLAYLISTS_DIR = SONGPLAYER_DIR.resolve("playlists");
+	public static final Path SONG_DIR = SONGPLAYER_DIR.resolve("songs");
 	public static FakePlayerEntity fakePlayer;
 
 	@Override
@@ -33,11 +38,19 @@ public class SongPlayer implements ModInitializer {
 			Util.createDirectoriesSilently(PLAYLISTS_DIR);
 		}
 
-		CommandProcessor.initCommands();
+		// Register StageTypes
+		StageTypeRegistry.instance.registerStageTypes(
+				new DefaultStage(),
+				new WideStage(),
+				new SphericalStage()
+		);
+
+		// Register commands
+		ClientCommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess) -> new SongPlayerCommand().registerCommand(dispatcher)));
 	}
 
 	public static void addChatMessage(String message) {
-		MC.player.sendMessage(Text.of(message), false);
+		addChatMessage(Text.of(message));
 	}
 
 	public static void addChatMessage(Text text) {
